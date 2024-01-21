97bd51628fcc327bc6d21ab13e98fc3944795f76
MrBisquit <william@wtdawson.info>
2024-01-21 11:50:05 +0000

    Add project files.

---
+++ TextObfuscator/MainWindow.xaml.cs
@@ -0,0 +1,79 @@
+﻿using Ookii.Dialogs.Wpf;
+using System.IO;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using TextObfuscator.Tools;
+
+namespace TextObfuscator
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            config = new Obfuscator.ObfuscationConfig(Array.Empty<string>(), true);
+        }
+
+        string SaveLocation = "";
+        string Unobfuscated = "";
+        string Obfuscated = "";
+
+        public static Obfuscator.ObfuscationConfig config;
+
+        private void SBSV_Checked(object sender, RoutedEventArgs e)
+        {
+            NViewTextBox.Visibility = Visibility.Collapsed;
+            SideBySideView.Visibility = Visibility.Visible;
+        }
+
+        private void SBSV_Unchecked(object sender, RoutedEventArgs e)
+        {
+            NViewTextBox.Visibility = Visibility.Visible;
+            SideBySideView.Visibility = Visibility.Collapsed;
+        }
+
+        private void FO_Click(object sender, RoutedEventArgs e)
+        {
+            VistaOpenFileDialog ofd = new VistaOpenFileDialog();
+            ofd.Title = "Text Obfuscator/Redacter - Select file";
+
+            if((bool)ofd.ShowDialog())
+            {
+                SaveLocation = ofd.FileName;
+                Unobfuscated = File.ReadAllText(ofd.FileName);
+                Update();
+            }
+        }
+
+        private void Update()
+        {
+            SBSN.Text = Unobfuscated;
+            Obfuscated = Obfuscator.Obfuscate(config, Unobfuscated);
+            SBSE.Text = Obfuscated;
+            NViewTextBox.Text = Obfuscated;
+        }
+
+        private void RO_Click(object sender, RoutedEventArgs e)
+        {
+            Update();
+        }
+
+        private void OCF_Click(object sender, RoutedEventArgs e)
+        {
+            CustomWindow customWindow = new CustomWindow();
+            customWindow.Show();
+        }
+    }
+}
---
+++ TextObfuscator/Tools/Obfuscator.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextObfuscator.Tools
+{
+    public static class Obfuscator
+    {
+        public class ObfuscationConfig
+        {
+            public string[] Filters;
+            public bool ObfuscateEmails;
+            public char ObfuscationChar;
+
+            public ObfuscationConfig(string[] filters, bool obfuscateEmails, char obfuscationChar = '▓')
+            {
+                Filters = filters;
+                ObfuscateEmails = obfuscateEmails;
+                ObfuscationChar = obfuscationChar;
+            }
+        }
+
+        public static char[] AvailableObfuscationChars = "▓▒░█■".ToCharArray();
+
+        public static string Obfuscate(ObfuscationConfig config, string text)
+        {
+            string obfuscated = "";
+            string[] split = text.Split(' '); // Split the string
+
+            if(config.ObfuscateEmails)
+            {
+                for (int i = 0; i < split.Length; i++)
+                {
+                    if(split[i].ToCharArray().Contains('@'))
+                    {
+                        split[i] = RepeatChar(config.ObfuscationChar, split[i].Length);
+                    }
+                }
+            }
+
+            for (int i = 0; i < config.Filters.Length; i++)
+            {
+                for (int j = 0; j < split.Length; j++)
+                {
+                    if(split[j].Contains(config.Filters[i]))
+                    {
+                        split[j] = RepeatChar(config.ObfuscationChar, split[j].Length);
+                    }
+                }
+            }
+
+            for (int i = 0; i < split.Length; i++)
+            {
+                obfuscated += split[i] + " ";
+            }
+
+            return obfuscated.TrimEnd();
+        }
+
+        public static string RepeatChar(char c, int r)
+        {
+            string repeated = "";
+
+            for (int i = 0; i < r; i++)
+            {
+                repeated += c;
+            }
+
+            return repeated;
+        }
+    }
+}
